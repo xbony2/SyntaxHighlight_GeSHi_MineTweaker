b91de2f371bf82bdd2cb0d9b515dc378752e955a
xbony2 <xbony22@gmail.com>
2015-09-27 08:50:55 -0400

    Made skeleton (I really don't know what I'm doing)

---
+++ SyntaxHighlight_GeSHi_MineTweaker.php
@@ -0,0 +1,16 @@
+<?php
+
+if(!defined('MEDIAWIKI'))
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+
+$wgShowExceptionDetails = true;
+
+$wgExtensionCredits['parserhooks'][] = array(
+		'path' => __FILE__,
+		'name' => 'SyntaxHighlight_GeSHi_MineTweaker',
+		'descriptionmsg' => 'syntaxhighlight-geshi-minetweaker-desc',
+		'version' => '1.0.0a',
+		'author' => '[http://ftb.gamepedia.com/User:Xbony2 Eric Schneider (xbony2)]',
+		'url' => 'http://help.gamepedia.com/Extension:SyntaxHighlight_GeSHi_MineTweaker',
+		'license-name' => 'MIT License'
+);
